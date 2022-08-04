1f623be0b8a47e1fd4920a22314a1b5b88e9329b
Liza <83118013+li3k4@users.noreply.github.com>
2022-08-04 12:17:00 +0200

    add(design, css)

--- ssl-landing/src/containers/App/App.jsx
+++ ssl-landing/src/containers/App/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import { useRef } from "react";
 import styles from "./App.module.css";
-import { NavLink } from "react-router-dom";
-import LocomotiveScroll from "locomotive-scroll";
 import {
   Animator,
   ScrollContainer,
@@ -21,116 +20,216 @@ import {
   ZoomOut,
 } from "react-scroll-motion";
 
+import Scroll from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
+
+import vim from "../../resources/img/vim.png";
+
 const ZoomScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
-const myScroll = new LocomotiveScroll();
 
 const App = () => {
   return (
     <ScrollContainer>
-      <ScrollPage page={0}>
-        <Animator animation={batch(StickyIn(), FadeIn(), Zoom(), Move(5, 5))}>
+      <ScrollPage page={1}>
+        <Animator animation={batch(StickyIn(), Fade())}>
           <div className={styles["item-first-page"]}>
-            <div className={styles[""]}>
-            <h1 data-scroll data-scroll-speed="1">
-              <span className={styles["s"]}>
-                Solar
-                <br />
-              </span>
-              <span className={styles["sl"]}>Snow Light</span>
-            </h1>
-            <p data-scroll data-scroll-speed="2" data-scroll-delay="0.2">
-              Создание вашего цифрового контена. Ваш контент, наши цифры.
-            </p>
-            </div>
-            <button className={styles["btn-contact"]}>
-              Contact 
+            <h1 className={styles["s"]}>Solar Snow Light</h1>
+            <button
+              onClick={() => scroll.scrollToBottom()}
+              className={styles["btn-contact"]}
+            >
+              Contact
             </button>
           </div>
         </Animator>
       </ScrollPage>
 
-      <ScrollPage page={1}>
-        <Animator animation={batch( FadeIn(), Sticky(), MoveIn(-1000, 0), MoveOut(0, -300))}>
-          <div>
-          <h1 className={styles["item-web"]}>Web</h1>
-          <h5>
-            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
-            tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrum exercitationem ullam corporis suscipit
-            laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute
-            iure reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt
-            in culpa qui officia deserunt mollit anim id est laborum.
-          </h5>
-          </div>
-
-        </Animator>
-      </ScrollPage>
-
       <ScrollPage page={2}>
-        <Animator animation={batch(MoveOut, MoveIn)}>
-          <div className={styles["item-netman"]}>
-            <h3>NetMan</h3>
-            <img
-              className={styles["item-netman-img"]}
-              src="https://cdn.dribbble.com/users/8794301/screenshots/18500021/media/1aa91ed564c3b908fdce188346c7550e.png?compress=1&resize=1000x750&vertical=top"
-            ></img>
+        <Animator animation={batch("StickyOut(), Fade()")}>
+          <div className={styles["page2"]}>
+            <div className={styles["item-2"]}>
+              <h1>15</h1>
+              <span>Веб-сайтов</span>
+            </div>
+            <div className={styles["item-2"]}>
+              <h1>9</h1>
+              <span>Проектов</span>
+            </div>
+            <div className={styles["item-2"]}>
+              <h1>2022</h1>
+              <span>Год основания</span>
+            </div>
+            <div className={styles["item-2"]}>
+              <h1>30</h1>
+              <span>Постоянных заказчиков</span>
+            </div>
           </div>
         </Animator>
       </ScrollPage>
 
       <ScrollPage page={3}>
-        <Animator animation={""}>
-          <div className={styles["item-5dim"]}>
-            <h3>5 Dimension</h3>
-            <img
-              className={styles["item-5dim-img"]}
-              src="https://cdn.dribbble.com/users/8794301/screenshots/17623582/media/9f75b4bbbc8335d5e8dc0162a6004a1a.png?compress=1&resize=1000x750&vertical=top"
-            ></img>
+        <Animator
+          animation={batch(
+            "FadeIn(), Sticky(), MoveIn(1000, 0), MoveOut(0, -300)"
+          )}
+        >
+          <div className={styles["table"]}>
+            <div className={styles["table-head"]}>
+              <h1>Разработка и поддержка сайтов</h1>
+              <span className={styles["table-head-sub"]}>
+                Создание сайтов любой сложности: от одностраничника-лендинга до
+                интернет-магазином со встроенной CRM-системой.
+              </span>
+            </div>
+
+            <ul className={styles["table-ul"]}>
+              <li className={styles["table-li"]}>
+                <div className={styles["table-li-span"]}>Ознакомление с продукцией и преймуществами вашей компании.{" "}</div>
+                <span>01</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Разработка технического задания.</div> <span>02</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Создание дизайна. Согласование.</div> <span>03</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Гибкая разработка с возможностью вносить правки.</div> <span>04</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Передача прав доступа владельцу.</div> <span>05</span>
+              </li>
+              <li className={styles["table-li-last"]}>
+              <div className={styles["table-li-span"]}>Поддержка сайта.</div><span>06</span>{" "}
+              </li>
+            </ul>
           </div>
         </Animator>
       </ScrollPage>
 
       <ScrollPage page={4}>
-        <Animator animation={batch( FadeIn(), Sticky(), MoveIn(1000,0), MoveOut(0, -300))}>
-          <h1>Desktop</h1>
-        </Animator>
-      </ScrollPage>
-
-      <ScrollPage page={5}>
-        <Animator animation={"batch(MoveIn(1000, 0), MoveOut())"}>
-          <div className={styles["item-5dim"]}>
-            <h3>5 Dimension</h3>
-            <img
-              className={styles["item-5dim-img"]}
-              src="https://cdn.dribbble.com/users/8794301/screenshots/18561221/media/a77896f92b7020f27ae39e6eb69bbcaa.png?compress=1&resize=1000x750&vertical=top"
-            ></img>
+        <Animator animation={batch( "FadeIn(), Sticky(), MoveIn(1000, 0), MoveOut(0, -300)"     )}
+        >
+          <div className={styles["page4"]}>
           </div>
         </Animator>
       </ScrollPage>
 
-      <ScrollPage>
+      <ScrollPage page={5}>
+        <Animator animation={""}>
+          <div className={styles["table"]}>
+            <div className={styles["table-head"]}>
+              <h1>Графический дизайн</h1>
+            </div>
+
+            <ul className={styles["table-ul"]}>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Контент для соцсетей.</div> <span>01</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Макет сайта.</div> <span>02</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Логотипы и брендбук (фирменный стиль Вашего бренда).{" "}</div>
+                <span>03</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Баннеры.</div> <span>04</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Презентации.</div> <span>05</span>
+              </li>
+              <li className={styles["table-li-last"]}>
+              <div className={styles["table-li-span"]}>Иллюстрации и изображения.</div><span>06</span>{" "}
+              </li>
+            </ul>
+          </div>
+        </Animator>
+      </ScrollPage>
+
+       <ScrollPage page={6}>
+        <Animator
+          animation={batch(
+            "FadeIn(), Sticky(), MoveIn(1000, 0), MoveOut(0, -300)"
+          )}
+        >
+          <div className={styles["page6"]}>
+              <h1 className={styles["mirror-word"]}> PANORAMA</h1>
+          </div>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={7}>
+        <Animator
+          animation={batch(
+            "FadeIn(), Sticky(), MoveIn(1000, 0), MoveOut(0, -300)"
+          )}
+        >
+          <div className={styles["table"]}>
+            <div className={styles["table-head"]}>
+              <h1>IT-аутсорсинг бизнеса</h1>
+              <span className={styles["table-head-sub"]}>Улучшим то, что уже есть, или создадим с нуля.</span>
+            </div>
+
+            <ul className={styles["table-ul"]}>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Подготовка и обслуживание рабочих мест сотрудников.{" "}</div>
+                <span>01</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}> Локальная внутрення сеть для предприятия .{" "}</div>
+                <span>02</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Защищенное облачное хранилище.</div> <span>03</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}>Подберка и настройка CRM-системы.</div> <span>04</span>
+              </li>
+              <li className={styles["table-li"]}>
+              <div className={styles["table-li-span"]}> Подберка техники под специфику Вашего бизнеса.</div><span>05</span>{" "}
+              </li>
+              <li className={styles["table-li-last"]}>
+              <div className={styles["table-li-span"]}> Личная и дистанционная техническая поддержка 24/7.</div>
+                <span>06</span>{" "}
+              </li>
+            </ul>
+          </div>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={8}>
+        <Animator
+          animation={batch(
+            "FadeIn(), Sticky(), MoveIn(1000, 0), MoveOut(0, -300)"
+          )}
+        >
+              <div className={styles["page8"]}> </div>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={9} ref={scroll}>
         <Animator>
-        <div className={styles["footer_down"]} id="down">
-      <a href="" className={styles["footer_email-btn"]}>
-        {" "}
-        SEND EMAIL
-      </a>
-      <div className={styles["footer"]}>
-        <div className={styles["item-footer"]}>
-          <h3>Контакты</h3>
-          <a href=" ">swdan.ov@gmail.ru</a>
-          <a>+79526139159</a>
-        </div>
-        <div className={styles["item-footer"]}>
-          <h3>Местоположение</h3>
-          <a href="https://www.google.pl/maps/place/%D1%83%D0%BB.+%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0+%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0,+13,+%D0%98%D1%80%D0%BA%D1%83%D1%82%D1%81%D0%BA,+%D0%98%D1%80%D0%BA%D1%83%D1%82%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F,+664074/@52.2568219,104.2641641,17z/data=!3m1!4b1!4m5!3m4!1s0x5da82352b361e3cf:0x250012cdb789df7d!8m2!3d52.2568219!4d104.2663528?hl=ru&authuser=0">
-            Россия, Иркутская область, г. Иркутск, <br /> ул. Академика
-            Курчатова 13
-          </a>
-        </div>
-      </div>
-    </div>
+          <div className={styles["footer_down"]}>
+            <a href="" className={styles["footer_email-btn"]}>
+              {" "}
+              SEND EMAIL
+            </a>
+            <div className={styles["footer"]}>
+              <div className={styles["item-footer"]}>
+                <h3>Контакты</h3>
+                <a href="">swdan.ov@gmail.ru</a>
+                <a>+79526139159</a>
+              </div>
+              <div className={styles["item-footer"]}>
+                <h3>Местоположение</h3>
+                <a href="https://www.google.pl/maps/place/%D1%83%D0%BB.+%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0+%D0%9A%D1%83%D1%80%D1%87%D0%B0%D1%82%D0%BE%D0%B2%D0%B0,+13,+%D0%98%D1%80%D0%BA%D1%83%D1%82%D1%81%D0%BA,+%D0%98%D1%80%D0%BA%D1%83%D1%82%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F,+664074/@52.2568219,104.2641641,17z/data=!3m1!4b1!4m5!3m4!1s0x5da82352b361e3cf:0x250012cdb789df7d!8m2!3d52.2568219!4d104.2663528?hl=ru&authuser=0">
+                  Россия, Иркутская область, г. Иркутск, <br /> ул. Академика
+                  Курчатова 13
+                </a>
+              </div>
+            </div>
+          </div>
         </Animator>
       </ScrollPage>
     </ScrollContainer>
